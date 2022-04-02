fd8fbcca736717a65ac40722a7e82dac1c057746
Hotmoon <99871157+hotmoon6@users.noreply.github.com>
2022-04-02 12:47:58 +0530

    Update ssgen.py

--- main/plugins/ssgen.py
+++ main/plugins/ssgen.py
@@ -68,7 +68,7 @@ async def screenshot(event, msg):
         return await edit.edit(f"An error occured while downloading.") 
     pictures = []
     captions = []
-    n = [27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1.5, 1.25, 1.10]
+    n = [28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1.5, 1.25, 1.10]
     duration = (video_metadata(name))["duration"]
     for i in range(30):
         sshot = await ssgen(name, duration/n[i]) 
